de59476d2ff310f6fdf5304c99d38ffcf1efeaca
Gwilyn <gwilyn@karmabunny.com.au>
2021-06-07 13:03:52 +0930

    Some docs for the action model.

--- src/Action.php
+++ src/Action.php
@@ -8,22 +8,35 @@ namespace karmabunny\router;
 
 
 /**
+ * This represents an action-able route.
+ *
+ * After finding a match, this object is produced. It contains the source
+ * elements (method, path, rule) and target elements (target, args).
  *
  * @package karmabunny\router
  */
 class Action
 {
 
+    /** @var string */
     public $method;
 
+    /** @var string */
     public $path;
 
-    public $target;
-
+    /** @var string */
     public $rule;
 
+    /** @var mixed */
+    public $target;
+
+    /** @var string[] */
     public $args;
 
+
+    /**
+     * @param array $config
+     */
     public function __construct($config)
     {
         foreach ($config as $key => $value) {
